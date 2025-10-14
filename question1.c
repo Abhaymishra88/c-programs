d381d15251b68916f927f9290ae427997dc92bcd
Abhaymishra88 <abhaymishramishra30@gmail.com>
2025-10-14 23:20:18 +0530

    Update question1.c

--- question1.c
+++ question1.c
@@ -6,9 +6,10 @@ int main()
     printf("enter mark of p,c,m,h,e");
     scanf("%d%d%d%d%d",&p,&c,&m,&h,&e);
     sum=p+c+m+h+e;
-    per=sum/5;
+    per=(float)sum/5;
     printf("sum  of p,c,m,h,e=%d",sum);
     printf("printf of per=%.2f",per);
     return 0;
 
 }
+
