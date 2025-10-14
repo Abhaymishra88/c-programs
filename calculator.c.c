0b7107e6f0db8f4d7c9b8bcdb6448ef3ac85f9a3
Abhaymishra88 <abhaymishramishra30@gmail.com>
2025-10-14 23:11:23 +0530

    Update calculator.c.c

--- calculator.c.c
+++ calculator.c.c
@@ -1,42 +1,43 @@
-#include<stdio.h>
-int main()
-{
-    int n ,a,b;
-    printf("enter 1 for sum\n,enter2 for sub\n,enter 3 for multi\n,enter 4 for divide,\n enter 5 for modulus");
-    scanf("%d",&n);
-    printf("enter two number"); 
-    scanf("%d%d",&a,&b);
-    switch(n){
+#include <stdio.h>
+
+int main() {
+    int n, a, b;
+
+    printf("Enter 1 for sum\n");
+    printf("Enter 2 for subtraction\n");
+    printf("Enter 3 for multiplication\n");
+    printf("Enter 4 for division\n");
+    printf("Enter 5 for modulus\n");
+    printf("Enter your choice: ");
+    scanf("%d", &n);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch(n) {
         case 1:
-        printf("sum is %d",a+b);
-        break;
-        case 2:
-        printf("sub is %d",a-b);
-        break;
-        case 3:
-        printf("multi is %d",a*b);
-        break;
-        case 4:
-        if (b==0){
-            printf("division is not possible")/n;
-        }
+            printf("Sum is %d\n", a + b);
             break;
-            case 5:
-                if (b==0){
-            
-            printf("modulus is not possible");
-        }
-        else{
-              printf("divide is%2f",a/b);
-        }
-             else{
-                printf("modulus is %2f",a%b==0);
-        }
-      break;
-      default:
-      printf("invalid");
-      return 0;
+        case 2:
+            printf("Subtraction is %d\n", a - b);
+            break;
+        case 3:
+            printf("Multiplication is %d\n", a * b);
+            break;
+        case 4:
+            if (b == 0) {
+                printf("Division is not possible (division by zero)\n");
+            } else {
+                printf("Division is %.2f\n", (float)a / b);
+                       if (b == 0) {
+                printf("Modulus is not possible (division by zero)\n");
+            } else {
+                printf("Modulus is %d\n", a % b);
+            }
+            break;
+        default:
+            printf("Invalid choice\n");
     }
 
-
+    return 0;
 }
