1bcccc4627e6910b24860534591874d17230d493
Abhaymishra88 <abhaymishramishra30@gmail.com>
2025-10-20 15:42:25 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,p;
+    printf("enter length a and width b=");
+    scanf("%d%d", &a,&b);
+    p=2*(a+b);
+    printf("the parameter is %d\n",p);
+    return 0;
+}
