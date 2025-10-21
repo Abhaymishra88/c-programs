03e5980050663089bbeaf370346026a4b02b89d6
Abhaymishra88 <abhaymishramishra30@gmail.com>
2025-10-21 08:00:36 +0530

    Add files via upload

---
+++ dig.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a num n=");
+    scanf("%d",&n);
+    if(n<=9)
+    {
+        printf("number is digit ");
+    }
+    else{
+        printf("number is not digit");
+    }
+}
