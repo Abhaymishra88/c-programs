cda0afe06d23d5f30d977356d790cda1f6eefbdd
Abhaymishra88 <abhaymishramishra30@gmail.com>
2025-10-21 06:53:37 +0530

    Add files via upload

---
+++ avg3num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a ,b,c,avg;
+    printf("enter three num a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    avg = (a+b+c)/3;
+    printf("avg of 3 num is %d",avg);
+}
